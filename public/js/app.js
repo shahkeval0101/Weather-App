19aa08f5eec3cdbf073481f1e6c0f6f743c0c6d7
shahkeval0101@gmail.com <shahkeval0101@gmail.com>
2022-05-23 02:50:59 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -37,7 +37,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('https://nodecourseudemy.kevalshah.repl.co/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
